b435931eb13226b25532356b85ddef5c32a05369
Maksim Kl <GuruHacker@yandex.ru>
2023-06-08 13:10:25 +0300

    Initial commit

---
+++ HomeWork-4.08.1.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+#include <vector>
+#include <memory>
+#include "tridiagonal_matrix.h"
+
+int main()
+{
+    auto down = std::vector<double>{ -4.0, 5.0 };
+    auto upper = std::vector<double>{ 14.0, 8.0 };
+    auto middle = std::vector<double>{ 3.0, 1.0, 7.0 };
+    auto matrix = std::make_unique<tridiagonal_matrix>(
+        down,
+        upper,
+        middle
+    );
+
+    auto matrix_clone = matrix->clone();
+
+    return 0;
+}
